a133a59250bb718b05a99a705d74c2d7a82fd1f3
NikaApts1auri <nika.aptsiauri77@gmail.com>
2025-09-20 18:54:36 +0400

    home work 3 complete

---
+++ Lec3/main.js
@@ -0,0 +1,41 @@
+//1
+// const numbers = [1, 2, 3, 4, 5];
+
+// function average() {
+//   const sum = numbers.reduce((prev, curr) => prev + curr, 0);
+//   return sum / numbers.length;
+// }
+
+// console.log(average());
+
+//2
+// const numbers = [1, 2, 3, 4, 5];
+
+// function reverse() {
+//   return numbers.reverse();
+// }
+// console.log(reverse()); // კარგად ვერ გავიაზრე კითხვა მგონი ამას გულისხმობდი
+
+//3
+// const a = [1, 2, 2, 2, 3];
+// const b = [2];
+
+// function difference(a, b) {
+//   return a.filter((item) => !b.includes(item));
+// }
+// console.log(difference([1, 2, 2, 2, 3], [2]));
+
+//4
+// const arr = [10, 40, 20, 5, 30];
+// function biggestSecond() {
+//   const sorted = arr.sort((a, b) => a - b);
+//   return sorted[sorted.length - 2];
+// }
+// console.log(biggestSecond());
+
+//5
+const arr = ["mom", "car", "level", "dog"];
+function isPalindrom() {
+  return arr.filter((word) => word === word.split("").reverse().join(""));
+}
+console.log(isPalindrom());
